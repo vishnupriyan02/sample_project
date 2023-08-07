de42bee3e346cdf838424f35e83cb22e8fa0d6d7
vishnupriyan1918 <vishnupriyan1918@gmail.com>
2023-08-07 22:38:36 +0530

    student detail added

---
+++ student.java
@@ -0,0 +1,30 @@
+
+class student{
+
+    String[] arr={"AjithKumar","venkat","vishnu"};
+
+    int[] id={01,02,03};
+
+    public void demo(String Name){
+       for (int i = 0; i < arr.length; i++) {
+           if(arr[i].equals(Name)){
+
+            System.out.println("id="+id[i]+"  Name="+arr[i]);
+           }
+           
+       }
+    }
+    public static void main(String[] args){
+
+       
+       student s=new student();
+      
+
+       String Name="vishnu";
+      
+
+       s.demo(Name);
+
+
+    }
+}
